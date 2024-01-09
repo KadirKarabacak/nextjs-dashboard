14dc999c1e45ac3dac3f734dc40bc1e66a20d7fa
Kadir Karabacak <kadirt@hotmail.com>
2024-01-09 17:50:57 +0300

    Created loading.tsx file for dashboard loading situation

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+// This one is a static file, sended before data arrives it shows loading.
+import DashboardSkeleton from '../../ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
