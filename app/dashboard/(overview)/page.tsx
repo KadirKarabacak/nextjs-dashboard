c2ed5b6e67ee50412696c69f890a527218366b1f
Kadir Karabacak <kadirt@hotmail.com>
2024-01-09 18:15:00 +0300

    Created a cardwrapper for cards and grouped and wrapped into suspense. now they are displaying alltogether

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,20 +2,20 @@
 
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import {
-  // fetchRevenue,  Deleted to suspense
-  // fetchLatestInvoices,  Deleted to suspense
-  fetchCardData,
-} from '@/app/lib/data';
+// import {
+//   fetchCardData,
+// } from '@/app/lib/data';
 
 // Use for suspense fetchRevenue
 import { Suspense } from 'react';
 import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
+  CardsSkeleton,
 } from '@/app/ui/skeletons';
 
-import { Card } from '@/app/ui/dashboard/cards';
+// import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import { lusitana } from '@/app/ui/fonts';
 
 // Page is an async component. This allows you to use await to fetch data.
@@ -25,12 +25,12 @@ export default async function Page() {
   // const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue()
 
   // The data requests are unintentionally blocking each other, creating a request waterfall.
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData(); // wait for fetchLatestInvoices()
+  // const {
+  //   numberOfCustomers,
+  //   numberOfInvoices,
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  // } = await fetchCardData(); // wait for fetchLatestInvoices()
 
   return (
     <main>
@@ -38,14 +38,17 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        />
+        /> */}
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {/* Now it is suspensed, do not blocking whole page. */}
