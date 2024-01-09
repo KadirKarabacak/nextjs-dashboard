cd79e37f30d553727adfebac6120231b1c5c5bc2
Kadir Karabacak <kadirt@hotmail.com>
2024-01-09 15:16:52 +0300

    Data fetched from database, imported fetchRev, fetchInvo, fetcCard datas and components displayed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,48 @@
 //! THIS PAGE IS UNIQUE FOR DASHBOARD, NO NEED TO ADD ANY UI INTO ROOT LAYOUT
-export default function Page() {
-  return <p>Dashboard Page</p>;
+
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from '@/app/lib/data';
+import { Card } from '@/app/ui/dashboard/cards';
+import { lusitana } from '@/app/ui/fonts';
+
+// Page is an async component. This allows you to use await to fetch data.
+export default async function Page() {
+  // Request waterfall
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue()
+
+  // The data requests are unintentionally blocking each other, creating a request waterfall.
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData(); // wait for fetchLatestInvoices()
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
