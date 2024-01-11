5c3f00783b6464879d68a1c0d66aafa1cc1a1329
Kadir Karabacak <kadirt@hotmail.com>
2024-01-11 14:50:48 +0300

    Root title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,18 @@
 import '@/app/ui/global.css';
 // Fonts
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+// Creates global Metadata, we can overwrite if we want to change it for spesific page
+export const metadata: Metadata = {
+  title: {
+    // % in the template will be replaced with the spesific title
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
