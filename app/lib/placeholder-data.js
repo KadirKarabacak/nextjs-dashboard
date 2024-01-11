904444d46cacbe69853bfbd1b8906ed8ce3d3448
Kadir Karabacak <kadirt@hotmail.com>
2024-01-11 15:28:03 +0300

    user data

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -15,15 +15,15 @@ const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
+    email: 'user@nextmail.com',
+    password: '123456',
+  },
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442b',
+    name: 'Admin',
     email: 'admin@nextmail.com',
     password: '654321',
   },
-  // {
-  //   id: '410544b2-4001-4271-9855-fec4b6a6442b',
-  //   name: 'Admin',
-  //   email: 'admin@nextmail.com',
-  //   password: '654321',
-  // },
 ];
 
 const customers = [
